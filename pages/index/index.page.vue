1cfa2e575c479d2ea45d8fc7039b9b9f19d33f74
JohnKougioum <73065650+JohnKougioum@users.noreply.github.com>
2022-07-17 23:46:23 +0300

    test

---
+++ pages/index/index.page.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Welcome</h1>
+  This page is:
+  <ul>
+    <li>Rendered to HTML.</li>
+    <li>Interactive. <Counter /></li>
+    <li v-for="item in todoList" :key="item.id">
+    <a :href="`/todos/${item.id}`">
+      {{ item.text }}
+    </a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import Counter from './Counter.vue'
+
+const todoList = [
+  {
+    id: 0,
+    text: 'Buy milk',
+  },
+  {
+    id: 1,
+    text: 'Buy chocolate',
+  },
+  {
+    id: 2,
+    text: 'Buy Chicken',
+  },
+  {
+    id: 3,
+    text: 'Buy Drinks',
+  },
+]
+</script>
---
+++ pages/todo/todo.page.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>To-do</h1>
+  <span>{{ todoId }}</span>
+  <br />
+  <a href="/">Back</a>
+</template>
+
+<script setup>
+import { usePageContext } from '../../renderer/usePageContext'
+
+const pageContext = usePageContext()
+const todoId = parseInt(pageContext.routeParams.todoId)
+
+</script>
